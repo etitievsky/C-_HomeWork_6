9533072dbd5aa059d004df16d2bf2bc1a57ea86f
etitievsky <etitievsky@mail.ru>
2023-01-01 22:53:18 +0300

    Task 01

---
+++ Task_01/Program.cs
@@ -0,0 +1,36 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+int EnterData(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int[] CreateArray(int number)
+{
+int[] array = new int [number];
+int count = 1;
+for (int i=0; i<number; i++)
+    {
+    array[i] = EnterData($"Введите число {count}: ");
+    count++;
+    }
+return array;
+}
+
+int CountPositiveNumbers(int[] arr)
+{
+int length = arr.Length;
+int count = 0;
+for (int i=0; i<length; i++)
+    {
+    if (arr[i] >0) count++;
+    }
+return count;
+}
+
+int quantityOfNumber = EnterData("Введите количество чисел для ввода: ");
+int[] massive = CreateArray(quantityOfNumber);
+int quantityPositiveNumbers = CountPositiveNumbers(massive);
+Console.WriteLine($"[{String.Join(",",massive)}]->{quantityPositiveNumbers}");
